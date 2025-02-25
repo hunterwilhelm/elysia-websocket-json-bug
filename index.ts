52d7ad86249cf5d1acbbe2d1b30d69e74d5a52eb
Hunter Wilhelm <71348224+hunterwilhelm@users.noreply.github.com>
2025-02-24 20:37:41 -0700

    add more JSON cases

--- index.ts
+++ index.ts
@@ -12,10 +12,13 @@ new Elysia()
   async function test() {
     const socket = new WebSocket("ws://localhost:3400/ws");
     socket.onopen = () => {
-      socket.send('{"id":1}');
-      socket.send('[{"id":1}]');
-      socket.send('{id:1}');
-      socket.send('[{id:1}]');
+      socket.send(JSON.stringify({"id":1}));
+      socket.send(JSON.stringify([{"id":1}]));
+      socket.send(JSON.stringify(true));
+      socket.send(JSON.stringify(false));
+      socket.send(JSON.stringify(0));
+      socket.send(JSON.stringify(null));
+      socket.send(JSON.stringify("Hello"));
     };
   }
   test();
